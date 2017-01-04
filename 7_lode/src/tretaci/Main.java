d16d7d8af96c11e7ea60a8ef3b5ed294cdf21771
Ondřej Krejčíř <ondrej.krejcir@dataspring.cz>
2017-01-04 16:17:27 +0100

    final tretaci

--- 7_lode/src/tretaci/Main.java
+++ 7_lode/src/tretaci/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package tretaci;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -7,23 +7,42 @@ public class Main {
 
 	private static int rozmer = 10;
 	private static String[][] hrac;
+	private static int hracZivoty;
 	private static String[][] pocitac;
+	private static int pocitacZivoty;
 
 	public static void main(String[] args) {
 		hrac = generujPole(rozmer);
-		pocitac = generujPole(rozmer);
-
 		vypisPole(hrac, "=== Hráč ===");
-
 		int[] coords = zadejSouradnice();
-		hrac = nasadTorpedoborce(hrac, coords[0], coords[1]);
-
+		nasadTorpedoborce(true, coords[0], coords[1]);
 		vypisPole(hrac, "=== Hráč ===");
-
 		coords = zadejSouradnice();
-		hrac = nasadPruzkumnika(hrac, coords[0], coords[1]);
-
+		nasadPruzkumnika(true, coords[0], coords[1]);
 		vypisPole(hrac, "=== Hráč ===");
+
+		pocitac = generujPole(rozmer);
+		coords = generujSouradnice();
+		nasadTorpedoborce(false, coords[0], coords[1]);
+		coords = generujSouradnice();
+		nasadPruzkumnika(false, coords[0], coords[1]);
+
+		while (hracZivoty > 0 && pocitacZivoty > 0) {
+			coords = zadejSouradnice();
+			strel(false, coords[0], coords[1]);
+			if (pocitacZivoty == 0) {
+				break;
+			}
+			coords = generujSouradnice();
+			strel(true, coords[0], coords[1]);
+			vypisPole(hrac, "=== Hráč ===");
+		}
+
+		if (hracZivoty > 0) {
+			System.out.println("Gratuluji hráči.");
+		} else {
+			System.out.println("Vyhrál počítač.");
+		}
 	}
 
 	private static int[] zadejSouradnice() {
@@ -43,14 +62,26 @@ public class Main {
 		return souradnice;
 	}
 
-	private static String[][] strel(String[][] pole, int x, int y) {
+	private static void strel(boolean proHrace, int x, int y) {
+		String[][] pole;
+		if (proHrace) {
+			pole = hrac;
+		} else {
+			pole = pocitac;
+		}
+
 		if (x < 0 || y < 0 || x > pole.length - 1 || y > pole.length - 1) {
 			System.out.println("Špatný vstup!");
-			return pole;
+			return;
 		}
 		switch(pole[x][y]) {
 			case "@":
 				pole[x][y] = "X";
+				if (proHrace) {
+					hracZivoty -= 1;
+				} else {
+					pocitacZivoty -=1;
+				}
 				break;
 			case ".":
 				pole[x][y] = "O";
@@ -58,29 +89,60 @@ public class Main {
 			default:
 				System.out.println("Na pole již byla provedena střelba!");
 		}
-		return pole;
+
+		if (proHrace) {
+			hrac = pole;
+		} else {
+			pocitac = pole;
+		}
 	}
 
 
-	private static String[][] nasadPruzkumnika(String[][] pole, int x, int y) {
+	private static void nasadPruzkumnika(boolean proHrace, int x, int y) {
+		String[][] pole;
+		if (proHrace) {
+			pole = hrac;
+		} else {
+			pole = pocitac;
+		}
+
 		if (x < 0 || y < 0 || x > pole.length - 1 || y > pole.length - 1) {
 			System.out.println("Špatný vstup!");
-			return pole;
+			return;
 		}
 		pole[x][y] = "@";
-		return pole;
+		if (proHrace) {
+			hrac = pole;
+			hracZivoty += 1;
+		} else {
+			pocitac = pole;
+			pocitacZivoty += 1;
+		}
 	}
 
-	private static String[][] nasadTorpedoborce(String[][] pole, int x, int y) {
+	private static void nasadTorpedoborce(boolean proHrace, int x, int y) {
+		String[][] pole;
+		if (proHrace) {
+			pole = hrac;
+		} else {
+			pole = pocitac;
+		}
+
 		if (x - 1 < 0 || y - 1 < 0 || x > pole.length - 1 || y + 1 > pole.length - 1) {
 			System.out.println("Špatný vstup!");
-			return pole;
+			return;
 		}
 		pole[x][y] = "@";
 		pole[x][y - 1] = "@";
 		pole[x][y + 1] = "@";
 		pole[x - 1][y] = "@";
-		return pole;
+		if (proHrace) {
+			hrac = pole;
+			hracZivoty += 4;
+		} else {
+			pocitac = pole;
+			pocitacZivoty += 4;
+		}
 	}
 
 	private static String[][] generujPole(int rozmer) {
